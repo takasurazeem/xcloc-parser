62d01eb8c57701bc3d00d36c0b5053fb603f2427
Takasur Azeem <takasurazeem@gmail.com>
2023-10-04 04:04:30 +0500

    Update main.py
    Mission abort. There's no free plan for open ai. My trial expired on April 23 without me realizing it.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import openai
+from api_keys import *
 from json_retriever import retrive_json
 
-print(retrive_json('/Users/takasurazeem/Desktop/QuranMajeed Localizations/ur.xliff'))
+json_data = retrive_json('/Users/takasurazeem/Desktop/QuranMajeed Localizations/ur.xliff')
+
+openai.api_key = f"{OPEN_AI_KEY}"
+
+# Define the prompt to be used with ChatGPT
+prompt = f"Translate the Target to Urdu carefully reading Note if any: \n{json_data}"
+# print(prompt)
+# exit(1)
+# Call the OpenAI GPT-3 API for translation
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=150,  # Adjust max_tokens as needed
+)
+
+# Extract and print the translated text from the API response
+translated_text = response.choices[0].text
+print("Translated Text (Urdu):")
+print(translated_text)
